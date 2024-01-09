388dd0fcf2284058c0c42112a26fcb43dcf08246
Jeffrey Vargas <jeffrey.vargas@hacom-tech.com>
2024-01-09 10:41:38 -0500

    Update

--- src/main/java/pe/sblm/intranet/IntranetApiApplication.java
+++ src/main/java/pe/sblm/intranet/IntranetApiApplication.java
@@ -2,9 +2,10 @@ package pe.sblm.intranet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class IntranetApiApplication {
+public class IntranetApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
         SpringApplication.run(IntranetApiApplication.class, args);
