b2592b0c91eaf9452531a59e52d4ae28e0db3aa4
sblmdev <jcayo@beneficenciadelima.org>
2024-01-11 16:16:30 -0500

    update

--- src/main/java/pe/sblm/intranet/model/Plan.java
+++ src/main/java/pe/sblm/intranet/model/Plan.java
@@ -7,11 +7,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Entity
+@Entity
 public class Plan {
-    /*@Id
+    @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @Column(length = 50)
+    private String nombre;
     
     @Column(length = 100)
     private String entidad;
@@ -32,7 +35,8 @@ public class Plan {
     	
     }
 
-	public Plan(String entidad, String informe, String fechaInforme, String tipoAuditoria, String entidadAuditora) {
+	public Plan(String nombre, String entidad, String informe, String fechaInforme, String tipoAuditoria, String entidadAuditora) {
+		this.nombre = nombre;
 		this.entidad = entidad;
 		this.informe = informe;
 		this.fechaInforme = fechaInforme;
@@ -46,6 +50,14 @@ public class Plan {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
 
 	public String getEntidad() {
@@ -86,6 +98,6 @@ public class Plan {
 
 	public void setEntidadAuditora(String entidadAuditora) {
 		this.entidadAuditora = entidadAuditora;
-	}*/
+	}
 
 }
