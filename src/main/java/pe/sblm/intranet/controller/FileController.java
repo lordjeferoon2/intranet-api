4222053fbc40fe98603482e9af0ec07b9a9161cc
sblmdev <jcayo@beneficenciadelima.org>
2024-01-16 16:55:04 -0500

    update

--- src/main/java/pe/sblm/intranet/controller/FileController.java
+++ src/main/java/pe/sblm/intranet/controller/FileController.java
@@ -1,5 +1,6 @@
 package pe.sblm.intranet.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import pe.sblm.intranet.model.Documento;
+import pe.sblm.intranet.repository.DocumentoRepositorio;
 
 import java.io.File;
 import java.io.IOException;
@@ -22,26 +26,31 @@ import java.util.UUID;
 @RequestMapping("/api/files")
 @CrossOrigin(origins = "*")
 public class FileController {
+	@Autowired
+    private DocumentoRepositorio documentoRepository;
 
 	@Value("${custom.url}")
     String uploadDir;
 
-    @PostMapping(value="/{tipo}")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String tipo) {
+    @PostMapping(value="/{tipo}/{idPublicacion}")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file, @PathVariable String tipo,  @PathVariable Long idPublicacion) {
         String uploadDir2 = "http://192.168.1.6/media";
         try {
             File directory = new File(uploadDir + "/" + tipo);
             if (!directory.exists()) {
-                directory.mkdirs(); // Utiliza mkdirs para crear directorios recursivamente si no existen
+                directory.mkdirs();
             }
 
             String fileName = generateUniqueFileName(file.getOriginalFilename());
             Path targetLocation = Paths.get(uploadDir + "/" + tipo).resolve(fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+            
+            Documento documento = new Documento(idPublicacion, uploadDir2 + "/" + tipo + "/" + fileName);
+            this.documentoRepository.save(documento);
 
             return ResponseEntity.ok(uploadDir2 + "/" + tipo + "/" + fileName);
         } catch (IOException ex) {
-            ex.printStackTrace(); // Imprime el stack trace del error para identificar la causa
+            ex.printStackTrace(); 
             return ResponseEntity.badRequest().body("Error uploading the file: " + ex.getMessage());
         }
     }
--- src/main/java/pe/sblm/intranet/controller/PublicacionController.java
+++ src/main/java/pe/sblm/intranet/controller/PublicacionController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import pe.sblm.intranet.model.Documento;
 import pe.sblm.intranet.model.Publicacion;
+import pe.sblm.intranet.repository.DocumentoRepositorio;
 import pe.sblm.intranet.repository.PublicacionRepositorio;
 
 import java.time.LocalDateTime;
@@ -19,6 +21,9 @@ public class PublicacionController {
 
     @Autowired
     private PublicacionRepositorio publicacionRepo;
+    
+    @Autowired
+    private DocumentoRepositorio documentoRepo;
 
     @PostMapping
     public ResponseEntity<Publicacion> crearPublicacion(@RequestBody Publicacion publicacion) {
@@ -66,21 +71,40 @@ public class PublicacionController {
 
 
     @GetMapping("/all/{gerencia}")
-    public List<Publicacion> obtenerTodasPublicaciones(@PathVariable String gerencia) {
-        return publicacionRepo.findAllByGerencia(gerencia);
-    }
-    
-    @GetMapping("/all/por-tipos/publicaciones")
-    public ResponseEntity<List<Publicacion>> obtenerPublicacionesPorTipos() {
-    	List<String> tipos = new ArrayList<String>();
-        List<Publicacion> publicaciones = publicacionRepo.findAllByTipoPublicacionIn(tipos);
-        return ResponseEntity.ok(publicaciones);
+    public ResponseEntity<List<Publicacion>> obtenerTodasPublicaciones(@PathVariable String gerencia) {
+    	List<Publicacion> publicaciones = publicacionRepo.findAllByGerencia(gerencia);
+    	for(int i=0; i<publicaciones.size(); i++) {
+    		List<Documento> documentos = documentoRepo.findAllByIdPublicacion(publicaciones.get(i).getId());
+    		publicaciones.get(i).setUrlDocumento(documentos.get(0).getUrlDocumento());
+    	}
+    	 return ResponseEntity.ok(publicaciones);
     }
 
     @GetMapping("/all/por-tipo/{tipo}")
     public ResponseEntity<List<Publicacion>> obtenerPublicacionesPorTipo(@PathVariable String tipo) {
         List<Publicacion> publicaciones = publicacionRepo.findAllByTipoPublicacion(tipo);
-        return ResponseEntity.ok(publicaciones);
+        for(int i=0; i<publicaciones.size(); i++) {
+        	List<Documento> documentos = documentoRepo.findAllByIdPublicacion(publicaciones.get(i).getId());
+    		publicaciones.get(i).setUrlDocumento(documentos.get(0).getUrlDocumento());
+    	}
+        
+        if(tipo.equals("Comunicaciones")) {
+            List<Publicacion> publicaciones2 = publicacionRepo.findAllByTipoPublicacion("Eventos");
+            for(int i=0; i<publicaciones2.size(); i++) {
+                List<Documento> documentos = documentoRepo.findAllByIdPublicacion(publicaciones2.get(i).getId());
+                publicaciones2.get(i).setUrlDocumento(documentos.get(0).getUrlDocumento());
+            }
+
+            List<Publicacion> publicaciones3 = publicacionRepo.findAllByTipoPublicacion("Galería");
+            for(int i=0; i<publicaciones3.size(); i++) {
+                List<Documento> documentos = documentoRepo.findAllByIdPublicacion(publicaciones3.get(i).getId());
+                publicaciones3.get(i).setUrlDocumento(documentos.get(0).getUrlDocumento());
+            }
+
+            publicaciones.addAll(publicaciones2);
+            publicaciones.addAll(publicaciones3);
+        }
+    	return ResponseEntity.ok(publicaciones);
     }
 
     @GetMapping("/{id}")
